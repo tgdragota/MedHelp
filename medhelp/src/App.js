9817f78af0c85fda3a0f141e1ce44b3c2336e73b
lauragiurea <laura.giurea@yahoo.com>
2022-11-25 20:51:29 +0200

    Programare

--- medhelp/src/App.js
+++ medhelp/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from "./components/Navbar";
+import {Route, Routes} from "react-router-dom";
+import Programare from "./pacient/Programare"
 
 function App() {
   return (
       <>
           <Navbar />
           <div className="App">
-
+              <Routes>
+                  <Route path='/programare' element={<Programare/>}></Route>
+              </Routes>
           </div>
       </>
 
--- medhelp/src/index.js
+++ medhelp/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import 'font-awesome/css/font-awesome.min.css';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
